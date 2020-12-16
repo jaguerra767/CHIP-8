15d191f4eec27e207ed2ca12c5cf0de22f27f0c9
Jorge Guerra <jorgeguerra@Jorges-MacBook.local>
2020-12-15 21:56:24 -0800

    added SDL platform

---
+++ platform.h
@@ -0,0 +1,22 @@
+#ifndef PLATFORM_H
+#define PLATFORM_H
+#include <cstdint>
+
+class SDL_Window;
+class SDL_Renderer;
+class SDL_Texture;
+
+class Platform
+{
+public:
+  Platform(char const* title, int windowW, int windownH, int textureW, int textureH);
+  ~Platform();
+  void Update(void const* buffer, int pitch);
+  bool ProcessInput(uint8_t* keys);
+
+private:
+  SDL_Window* window{};
+  SDL_Rendere* renderer{};
+  SDL_Texture* texture{};
+};
+#endif
